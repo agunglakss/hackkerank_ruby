1b6b9d661d4fc41bde4e07d57cad75ebcbf6c69e
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2024-09-23 14:02:52 +0700

    Fix: issue to range between start poing and end point

--- count_apples_and_oranges.rb
+++ count_apples_and_oranges.rb
@@ -1,7 +1,7 @@
 # Hackerrank
 # Day 3 of 300
 # https://www.hackerrank.com/challenges/apple-and-orange
-# Solution: first we need to counting each element of apples then iterate to check if range apple tree and orange tree are inside the house
+# Solution: first we need to counting each element of apples and oranges then iterate to check if apple and orange is falling between range the house
 
 def count_apples_and_oranges(s, t, a, b, apples, oranges)
   rangeApples = apples.map {|apple| a + apple }
@@ -11,14 +11,12 @@ def count_apples_and_oranges(s, t, a, b, apples, oranges)
   total_orange = 0
 
   rangeApples.each do |total_range|
-    total_apple = total_apple + 1 if total_range >= s
+    total_apple = total_apple + 1 if total_range >= s and total_range <= t
   end
 
   rangeOranges.each do |total_range|
-    total_orange = total_orange + 1 if total_range <= t
+    total_orange = total_orange + 1 if total_range <= t and total_range >= s
   end
 
   return total_apple, total_orange 
 end
-
-self.count_apples_and_oranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4])
